fe9a4f1f376d5dbc078b8d62ef00bb35b835f3da
Giuseppe Silvestro <giuseppe.silvestro@live.com>
2020-11-14 12:07:27 +0000

    Add Thymeleaf library to handle HTML files. Add templates folder in the resources folder to contain the HTML files for our project.

--- src/main/java/com/giuseppesilvestro/giflib/controller/GifController.java
+++ src/main/java/com/giuseppesilvestro/giflib/controller/GifController.java
@@ -2,14 +2,14 @@ package com.giuseppesilvestro.giflib.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class GifController {
     //home page. As there is only value, you can omit it
     @RequestMapping(value = "/")
-    @ResponseBody
     public String listGifs() {
-        return "List of all the GIFs!";
+        //as the home.html file is in the right folder (resources/templates),
+        // our framework will recognize it and display it
+        return "home";
     }
 }
