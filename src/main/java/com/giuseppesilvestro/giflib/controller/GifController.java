15982196386d5f7e405b2539289dc0c2ffd2d23c
Giuseppe Silvestro <giuseppe.silvestro@live.com>
2020-11-16 10:30:22 +0000

    Use @PathVariable to make our Detail Page dynamic.
    Fix the "mark as favourite" option for our gifs adding a ternary operator in the gif-details.html
    
    Change some tags in gif-details.html using th:text to dinamically change the info displayed by the page according to the different gifs.

--- src/main/java/com/giuseppesilvestro/giflib/controller/GifController.java
+++ src/main/java/com/giuseppesilvestro/giflib/controller/GifController.java
@@ -5,6 +5,7 @@ import com.giuseppesilvestro.giflib.model.Gif;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDate;
@@ -30,9 +31,9 @@ public class GifController {
         return "home";
     }
 
-    @RequestMapping("/gif")
-    public String gifDetails(ModelMap modelMap){
-        Gif gif = gifRepository.findByName("android-explosion");
+    @RequestMapping("/gif/{name}")
+    public String gifDetails(@PathVariable String name, ModelMap modelMap){
+        Gif gif = gifRepository.findByNameFunctionally(name);
         modelMap.put("gif", gif);
         return "gif-details";
     }
--- src/main/java/com/giuseppesilvestro/giflib/data/GifRepository.java
+++ src/main/java/com/giuseppesilvestro/giflib/data/GifRepository.java
@@ -11,12 +11,12 @@ import java.util.Optional;
 @Component
 public class GifRepository {
     private static final List<Gif> ALL_GIFS = Arrays.asList(
-            new Gif("android-explosion", LocalDate.of(2015,2,13), "Chris Ramacciotti", false),
-            new Gif("ben-and-mike", LocalDate.of(2015,10,30), "Ben Jakuben", true),
-            new Gif("book-dominos", LocalDate.of(2015,9,15), "Craig Dennis", false),
-            new Gif("compiler-bot", LocalDate.of(2015,2,13), "Ada Lovelace", true),
-            new Gif("cowboy-coder", LocalDate.of(2015,2,13), "Grace Hopper", false),
-            new Gif("infinite-andrew", LocalDate.of(2015,8,23), "Marissa Mayer", true)
+            new Gif("android-explosion", LocalDate.of(2015, 2, 13), "Chris Ramacciotti", false),
+            new Gif("ben-and-mike", LocalDate.of(2015, 10, 30), "Ben Jakuben", true),
+            new Gif("book-dominos", LocalDate.of(2015, 9, 15), "Craig Dennis", false),
+            new Gif("compiler-bot", LocalDate.of(2015, 2, 13), "Ada Lovelace", true),
+            new Gif("cowboy-coder", LocalDate.of(2015, 2, 13), "Grace Hopper", false),
+            new Gif("infinite-andrew", LocalDate.of(2015, 8, 23), "Marissa Mayer", true)
     );
 
     public Gif findByName(String name) {
@@ -29,4 +29,10 @@ public class GifRepository {
         return null;
     }
 
+    public Gif findByNameFunctionally(String name) {
+        return ALL_GIFS.stream()
+                .filter(e -> e.getName().equals(name))
+                .findFirst()
+                .orElse(null);
+    }
 }
