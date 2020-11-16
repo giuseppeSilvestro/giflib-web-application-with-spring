28b4a4f4509ff8995a642061c2b77883cf576fe9
Giuseppe Silvestro <giuseppe.silvestro@live.com>
2020-11-16 09:56:07 +0000

    Create a new package where to store our GifRepository class: in this class we are going to create a List of gifs that we can use to display on our webpage.
    Use Dependency Injection.
    
    Use a method of GifRepository to find the gif we are going to display.

--- src/main/java/com/giuseppesilvestro/giflib/controller/GifController.java
+++ src/main/java/com/giuseppesilvestro/giflib/controller/GifController.java
@@ -1,6 +1,8 @@
 package com.giuseppesilvestro.giflib.controller;
 
+import com.giuseppesilvestro.giflib.data.GifRepository;
 import com.giuseppesilvestro.giflib.model.Gif;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +11,17 @@ import java.time.LocalDate;
 
 @Controller
 public class GifController {
+    /*
+    Spring has a function that can initialize fields for us when we need them in the code, but there is a condition:
+    it needs to find a component of the same class for the field.
+    To use this function we have the @Autowired.
+    To tell Spring where to look for the component we use the @Component annotation
+    (in this case, in the GifRepository class).
+    This process of assigning objects without explicitly writing any code is called: Dependency Injection, or DI.
+     */
+    @Autowired
+    private GifRepository gifRepository;
+
     //home page. As there is only value, you can omit it
     @RequestMapping(value = "/")
     public String listGifs() {
@@ -19,8 +32,7 @@ public class GifController {
 
     @RequestMapping("/gif")
     public String gifDetails(ModelMap modelMap){
-        Gif gif = new Gif("android-explosion",
-                LocalDate.of(2015, 2, 13), "Giuseppe", true);
+        Gif gif = gifRepository.findByName("android-explosion");
         modelMap.put("gif", gif);
         return "gif-details";
     }
